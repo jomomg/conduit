6315cbe82c5b02cbaecade35c13717d9ee6cd1b1
jomomg <jomomg@gmail.com>
2023-06-23 15:21:05 +0300

    add api app to installed apps in settings

--- conduit/settings.py
+++ conduit/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'conduit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'conduit',
+        'USER': 'dev_user',
+        'PASSWORD': 'dev_password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
